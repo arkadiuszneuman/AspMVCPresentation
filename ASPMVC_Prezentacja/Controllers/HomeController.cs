0f168d180e96a02bbcda6486f3fe07e04b29d06d
Arkadiusz Neuman <mrmoro@o2.pl>
2015-01-06 13:55:45 +0100

    Init

---
+++ ASPMVC_Prezentacja/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC_Prezentacja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HelloName(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
